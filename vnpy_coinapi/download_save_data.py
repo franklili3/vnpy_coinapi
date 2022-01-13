8647d41c47046353ad74401b284a5f20124ec8de
franklili <348104201@qq.com>
2022-01-13 09:24:52 +0800

    add save contract data

--- vnpy_coinapi/download_save_data.py
+++ vnpy_coinapi/download_save_data.py
@@ -29,8 +29,7 @@ datafeed = CoinapiDatafeed()
 database = get_database()
 
 # 下载历史数据
-contracts_list = []
-contract_dict = {}
+contracts = []
 for symbol_dict in symbols:
 
     # 构建请求结构体
@@ -50,4 +49,31 @@ for symbol_dict in symbols:
     print('symbol_id: ', symbol_dict['symbol_id'], ' count: ', len(bars_data))
 
     # 构建合约数据
-    contract_dict['symbol'] = 
+    contract_dict = {}
+    symbol_split = symbol_dict['smybol_id'].split('_')
+    contract_dict['symbol'] = symbol_split[2] + '_' + symbol_split[3] + '_' + symbol_split[4] + '_' + symbol_split[5] + '_' + symbol_split[6]
+    contract_dict['exchange'] = Exchange(symbol_dict['exchange_id'])
+    if symbol_split[4] == 'C':
+        contract_dict['name'] = 'Bitcoin' + '_' + symbol_split[3] + '_' + symbol_split[4] + '_' + symbol_split[5] + '_' + 'CALL'
+    else:
+        contract_dict['name'] = 'Bitcoin' + '_' + symbol_split[3] + '_' + symbol_split[4] + '_' + symbol_split[5] + '_' + 'PUT'
+    contract_dict['product'] = symbol_dict['symbol_type']
+    contract_dict['size'] = symbol_dict['option_contract_unit']
+    contract_dict['pricetick'] = symbol_dict['price_precision']
+    contract_dict['min_volume'] = symbol_dict['size_precision']
+    contract_dict['stop_supported'] = True
+    contract_dict['net_position'] = False
+    contract_dict['history_data'] = True
+    contract_dict['option_strike'] = symbol_dict['option_strike_price']
+    contract_dict['option_underlying'] = symbol_split[2] + '_' + symbol_split[3] + '_' + symbol_split[4]
+    if symbol_dict['option_type_is_call'] == True:
+        contract_dict['option_type'] = 'CALL'
+    else:
+        contract_dict['option_type'] = 'PUT'
+    contract_dict['option_listed'] = datetime.datetime.strptime(symbol_dict['data_start'], '%Y-%m-%d')
+    contract_dict['option_expiry'] = datetime.datetime.strptime(symbol_dict['option_expiration_time'], '%Y-%m-%dT%h:%m:%sZ')
+    contract_dict['option_portfolio'] = ""
+    contract_dict['option_index'] = ""
+    contracts.append(contract_dict)
+database.save_contract_data(contracts)
+print('contracts count: ', len(contracts))
